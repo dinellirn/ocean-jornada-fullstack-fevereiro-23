f937ad8bcf8bb2f53080865e49caf7cfdbaf5138
dinellirn <dinelli.rn@gmail.com>
2023-02-16 11:08:46 -0400

    Implementação do ReadAll

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,11 @@
 import './App.css'
+import ReadAll from './components/ReadAll/ReadAll'
 
 function App() {
 
   return (
     <div className="App">
-      Hello, world JSX!
+      <ReadAll />
     </div>
   )
 }
