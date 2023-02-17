8e98d4b4cb5ca12d43d8eabfedc1d69b03cffc43
dinellirn <dinelli.rn@gmail.com>
2023-02-17 08:38:52 -0400

    Implementação do card no ReadAll

---
+++ frontend/src/components/Card/Card.jsx
@@ -0,0 +1,10 @@
+function Card() {
+    return (      
+    <div className="card">
+    <h1>Mobius One</h1>
+    <img src="https://scontent.fmao1-1.fna.fbcdn.net/v/t1.6435-9/84874192_1399945626844174_643629286284066816_n.png?_nc_cat=103&ccb=1-7&_nc_sid=730e14&_nc_eui2=AeGvc8TSUpClBoNTClh4pcLiTD4wizRK7JtMPjCLNErsmzn2FPpcaMpRARZi-VO-Jo9r3hNYbqCZp37mWQhYpHnT&_nc_ohc=FLej0tluRl0AX943iL4&_nc_ht=scontent.fmao1-1.fna&oh=00_AfC9EJlwjs3yqbwwF5hEo-aG1rEzIlNwXBdCqeEAQq4RkQ&oe=6416D6CB" alt="Mobius One" />
+  </div>
+  )
+}
+
+export default Card;
